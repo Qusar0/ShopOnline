6603101fe279f9e4b048b4f40785742d073afafb
Qusar0 <artyom090123@gmail.com>
2024-03-26 23:37:14 +0300

    Add project files.

---
+++ ShopOnline.Api/Entitites/ProductCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopOnline.Api.Entitites
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string IconCSS { get; set; }
+    }
+}
